bbdb9cb00726fefa62241ea0cbec3b610e1f1868
Tristan Gilford <gilford.tristan@Tristans-MacBook-Pro.local>
2025-02-19 21:59:03 -0700

    added ALB

--- main.tf
+++ main.tf
@@ -28,6 +28,9 @@ resource "aws_launch_template" "tf-example" {
 resource "aws_autoscaling_group" "tf-example" {
     vpc_zone_identifier  = data.aws_subnets.default.ids
 
+    target_group_arns = [aws_alb_target_group.asg.arn]
+    health_check_type = "ELB"
+
     min_size = 2
     max_size = 10
 
@@ -41,6 +44,85 @@ resource "aws_autoscaling_group" "tf-example" {
       value               = "terraform-asg-example"
       propagate_at_launch = true
     }
+}
+
+# ALB for ASG
+resource "aws_lb" "tf-example" {
+    name                = "terraform-asg-example"
+    load_balancer_type  = "application"
+    subnets             = data.aws_subnets.default.ids
+    security_groups     = [aws_security_group.alb.id]
+}
+
+# Listener for ALB
+resource "aws_lb_listener" "http" {
+    load_balancer_arn   = aws_lb.tf-example.arn
+    port                = 80
+    protocol            = "HTTP"
+
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "404: page not found"
+        status_code  = 404
+      }
+    }
+}
+
+# ALB Target Group for ASG
+resource "aws_alb_target_group" "asg" {
+  name      = "terraform-asg-example"
+  port      = var.server_port
+  protocol  = "HTTP"
+  vpc_id    = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Listener Rules for ALB
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.asg.arn
+  }
+}   
+
+# Security Group for ALB Traffic
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -70,7 +152,7 @@ data "aws_subnets" "default" {
   }
 }
 
-# output "public_ip" {
-#   value         = aws_instance.tf-example.public_ip
-#   description   = "The public IP address of the instance"
-# }
+output "alb_dns_name" {
+  value         = aws_lb.tf-example.dns_name
+  description   = "The public IP address of the load balancer"
+}
